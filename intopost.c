2e5ae2e0747f24bb5f1adfd642c6809f2e506815
Rahini Raneru <rahiniraneru@yahoo.com>
2025-11-14 13:09:44 +0530

    infixtopost

--- intopost.c
+++ intopost.c
@@ -26,3 +26,45 @@ int precedence(char c) {
         return -1;
 }
 
+void infixToPost(char infix[], char postfix[]) {
+
+    int k = 0;
+    for (int i = 0; infix[i]; i++) {
+        char c = infix[i];
+        if (isalnum(c)) {
+            postfix[k++] = c;
+        }
+        else if (c == '(') {
+            push(c);
+        }
+        else if (c == ')') {
+            while (top != -1 && stack[top] != '(') {
+                postfix[k++] = pop();
+            }
+            pop();
+        }
+        else {
+            while (top != -1 && precedence(stack[top]) >= precedence(c)) {
+                postfix[k++] = pop();
+            }
+            push(c);
+        }
+    }
+
+    while (top != -1) {
+        postfix[k++] = pop();
+    }
+
+    postfix[k] = '\0';
+}
+
+int main() {
+    char infix[MAX], postfix[MAX];
+    printf("Enter infix expression: ");
+    gets(infix);
+
+    infixToPost(infix, postfix);
+    printf("Postfix Expression: %s\n", postfix);
+
+    return 0;
+}
--- test.c
+++ test.c
@@ -1,10 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main () {
-    int x = 20, *y;
-    y = &x;
-    *y++;
-    x++;
-    printf("%d %d",x,y);
+typedef struct Node {
+    int data;
+    struct Node *left, *right;
+} Node;
+
+Node* createNode (int val) {
+    Node* newNode = (struct Node*)malloc(sizeof(Node));
+    newNode->data = val;
+    newNode->left= newNode->right = NULL;
+    return newNode;
+}
+
+Node* insert(struct Node* root, int val) {
+    if (root == NULL) {
+        return createNode(val);
+    }
+    else if (val < root->data) {
+        root->left = insert(root->left, val);
+    }
+    else {
+        root->right = insert(root->right, val);
+    }
+    return root;
+}
+
+void inorder(Node* root) {
+    if (root != NULL) {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+void postorder(Node* root) {
+    if (root != NULL) {
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+void preorder(Node* root) {
+    if (root != NULL) {
+        printf("%d ", root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
 }
